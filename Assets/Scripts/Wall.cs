77083de31476825bae66ced77d285d935fb22d64
Marcus Elg <marcusaccounts@yahoo.se>
2021-12-21 10:22:22 +0100

    Require components

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
 
+[RequireComponent(typeof(SplineContainer), typeof(MeshRenderer), typeof(MeshFilter))]
 public class Wall : MonoBehaviour
 {
 
