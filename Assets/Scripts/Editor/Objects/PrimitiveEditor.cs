f0671c60cbc916911876cbe199b040d4422da69f
Marcus Elg <marcusaccounts@yahoo.se>
2021-12-26 16:55:53 +0100

    Add primitive handle at min position

--- Assets/Scripts/Editor/Objects/PrimitiveEditor.cs
+++ Assets/Scripts/Editor/Objects/PrimitiveEditor.cs
@@ -12,6 +12,7 @@ namespace BlockoutTools
         private void OnEnable()
         {
             primitive = (Primitive)target;
+            Tools.current = Tool.None;
         }
 
         private void OnSceneGUI()
@@ -24,27 +25,32 @@ namespace BlockoutTools
         private void Draw()
         {
             // Dragging handles requires more than just draw event
-            // Bounds position handle
+            // Bounds position handles
             {
-                EditorGUI.BeginChangeCheck();
-                Vector3 bounds = primitive.GetComponent<MeshFilter>().sharedMesh.bounds.max;
-                Vector3 currentBoundsHandlePosition = primitive.transform.TransformPoint(bounds);
-                Vector3 newBoundsHandlePosition = CustomHandles.DrawPositionHandle(false, settings.FindProperty("gizmoSize").floatValue * 5, currentBoundsHandlePosition, Quaternion.LookRotation(primitive.transform.TransformDirection(Vector3.forward)));
+                ScaleHandle(primitive.GetComponent<MeshFilter>().sharedMesh.bounds.min);
+                ScaleHandle(primitive.GetComponent<MeshFilter>().sharedMesh.bounds.max);
+            }
+        }
 
-                if (EditorGUI.EndChangeCheck())
-                {
-                    Vector3 newLocalPosition = primitive.transform.InverseTransformPoint(newBoundsHandlePosition);
-                    // Only apply half of the scaling as the movement otherwise will make it scale twice as much in that direction
-                    float xScale = Mathf.Lerp(1, newLocalPosition.x / bounds.x, 0.5f) * primitive.transform.localScale.x;
-                    float yScale = Mathf.Lerp(1, newLocalPosition.y / bounds.y, 0.5f) * primitive.transform.localScale.y;
-                    float zScale = Mathf.Lerp(1, newLocalPosition.z / bounds.z, 0.5f) * primitive.transform.localScale.z;
+        private void ScaleHandle(Vector3 bounds)
+        {
+            EditorGUI.BeginChangeCheck();
+            Vector3 currentBoundsHandlePosition = primitive.transform.TransformPoint(bounds);
+            Vector3 newBoundsHandlePosition = CustomHandles.DrawPositionHandle(false, settings.FindProperty("gizmoSize").floatValue * 5, currentBoundsHandlePosition, Quaternion.LookRotation(primitive.transform.TransformDirection(Vector3.forward)));
 
-                    Vector3 movement = new Vector3(xScale - primitive.transform.localScale.x, yScale - primitive.transform.localScale.y, zScale - primitive.transform.localScale.z);
-                    movement.Scale(bounds); // Scale with bounds to work the same regardless of bound size
-                    primitive.transform.localScale = new Vector3(xScale, yScale, zScale);
-                    primitive.transform.position += primitive.transform.rotation * movement;
-                    primitive.GetComponent<MeshFilter>().sharedMesh.RecalculateBounds();
-                }
+            if (EditorGUI.EndChangeCheck())
+            {
+                Vector3 newLocalPosition = primitive.transform.InverseTransformPoint(newBoundsHandlePosition);
+                // Only apply half of the scaling as the movement otherwise will make it scale twice as much in that direction
+                float xScale = Mathf.Lerp(1, newLocalPosition.x / bounds.x, 0.5f) * primitive.transform.localScale.x;
+                float yScale = Mathf.Lerp(1, newLocalPosition.y / bounds.y, 0.5f) * primitive.transform.localScale.y;
+                float zScale = Mathf.Lerp(1, newLocalPosition.z / bounds.z, 0.5f) * primitive.transform.localScale.z;
+
+                Vector3 movement = new Vector3(xScale - primitive.transform.localScale.x, yScale - primitive.transform.localScale.y, zScale - primitive.transform.localScale.z);
+                movement.Scale(bounds); // Scale with bounds to work the same regardless of bound size
+                primitive.transform.localScale = new Vector3(xScale, yScale, zScale);
+                primitive.transform.position += primitive.transform.rotation * movement;
+                primitive.GetComponent<MeshFilter>().sharedMesh.RecalculateBounds();
             }
         }
     }
