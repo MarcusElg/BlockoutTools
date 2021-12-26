4cba688694d9c0630d8f27b1ef8b6cf2f3bae611
Marcus Elg <marcusaccounts@yahoo.se>
2021-12-26 14:30:46 +0100

    Fix spiral stair generation

--- Assets/Scripts/Editor/Objects/SpiralStairsEditor.cs
+++ Assets/Scripts/Editor/Objects/SpiralStairsEditor.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 [CustomEditor(typeof(SpiralStairs))]
 public class SpiralStairEditor : Editor
 {
-    Stairs stairs = null;
+    SpiralStairs stairs = null;
     SerializedObject settings;
 
     private void OnEnable()
     {
-        stairs = (Stairs)target;
+        stairs = (SpiralStairs)target;
 
         // Regenerate on changes
         Undo.undoRedoPerformed += stairs.Generate;
@@ -47,26 +47,11 @@ public class SpiralStairEditor : Editor
     public override void OnInspectorGUI()
     {
         EditorGUI.BeginChangeCheck();
-        stairs.type = (Stairs.Type)EditorGUILayout.EnumPopup("Type", stairs.type);
-        stairs.depth = EditorGUILayout.FloatField("Depth", stairs.depth);
-
-        if (stairs.type == Stairs.Type.Spiral)
-        {
-            stairs.innerRadius = EditorGUILayout.FloatField("Inner Radius", stairs.innerRadius);
-        }
-
+        stairs.innerRadius = EditorGUILayout.FloatField("Inner Radius", stairs.innerRadius);
         stairs.width = EditorGUILayout.FloatField("Width", stairs.width);
         stairs.height = EditorGUILayout.FloatField("Height", stairs.height);
-
-        if (stairs.type == Stairs.Type.Straight)
-        {
-            stairs.targetPosition = EditorGUILayout.Vector3Field(new GUIContent("Target Position", "Position for the stairs to generate towards in local space"), stairs.targetPosition);
-        }
-        else
-        {
-            stairs.rotateClockwise = EditorGUILayout.Toggle("Rotate Clockwise", stairs.rotateClockwise);
-            stairs.targetRotation = EditorGUILayout.FloatField("Target Rotation", stairs.targetRotation);
-        }
+        stairs.rotateClockwise = EditorGUILayout.Toggle("Rotate Clockwise", stairs.rotateClockwise);
+        stairs.targetRotation = EditorGUILayout.FloatField("Target Rotation", stairs.targetRotation);
 
         if (EditorGUI.EndChangeCheck() || GUILayout.Button("Generate"))
         {
@@ -77,22 +62,8 @@ public class SpiralStairEditor : Editor
     private void Draw()
     {
         // Dragging handles requires more than just draw event
-        // Target position handle
-        if (stairs.type == Stairs.Type.Straight)
+        // Rotation target handle
         {
-            EditorGUI.BeginChangeCheck();
-            Vector3 currentTargetHandlePosition = stairs.transform.position + stairs.targetPosition;
-            Vector3 newTargetHandlePosition = CustomHandles.DrawPositionHandle(false, settings.FindProperty("gizmoSize").floatValue * 5, currentTargetHandlePosition, Quaternion.identity);
-
-            if (EditorGUI.EndChangeCheck())
-            {
-                stairs.targetPosition = newTargetHandlePosition - stairs.transform.position;
-                stairs.Generate();
-            }
-        }
-        else
-        {
-            // Rotation target handle
             EditorGUI.BeginChangeCheck();
             Quaternion rotation = Handles.Disc(Quaternion.Euler(0, stairs.transform.rotation.eulerAngles.y + stairs.targetRotation, 0), stairs.transform.position, stairs.transform.TransformDirection(Vector3.up), stairs.innerRadius + stairs.width, false, EditorSnapSettings.rotate);
 
@@ -103,66 +74,44 @@ public class SpiralStairEditor : Editor
             }
         }
 
-        float widthOffset;
+        float widthOffset = stairs.innerRadius + stairs.width / 2;
         // Width handle
         {
             EditorGUI.BeginChangeCheck();
-            widthOffset = stairs.type == Stairs.Type.Straight ? -stairs.width / 2 : stairs.innerRadius;
-            Vector3 widthHandlePosition = stairs.transform.TransformPoint(Vector3.right * (stairs.width + widthOffset) + Vector3.up * stairs.height / 2 + Vector3.forward * stairs.depth / 2); // Convert to global space
+            Vector3 widthHandlePosition = stairs.transform.TransformPoint(Vector3.right * (stairs.width + stairs.innerRadius) + Vector3.up * stairs.height / 2); // Convert to global space
             widthHandlePosition = Handles.Slider(widthHandlePosition, stairs.transform.TransformDirection(Vector3.right), settings.FindProperty("gizmoSize").floatValue, CustomHandles.DiscCapFunction, EditorSnapSettings.move.x);
 
             if (EditorGUI.EndChangeCheck())
             {
-                stairs.width = (stairs.transform.InverseTransformPoint(widthHandlePosition) - Vector3.up * stairs.height / 2 - Vector3.forward * stairs.depth / 2 - Vector3.right * widthOffset).magnitude; // Convert to local space
+                stairs.width = (stairs.transform.InverseTransformPoint(widthHandlePosition) - Vector3.up * stairs.height / 2 - Vector3.right * stairs.innerRadius).magnitude; // Convert to local space
                 stairs.Generate();
             }
         }
 
         // Inner radius handle
-        if (stairs.type == Stairs.Type.Spiral)
         {
             EditorGUI.BeginChangeCheck();
-            Vector3 radiusHandlePosition = stairs.transform.TransformPoint(Vector3.right * stairs.innerRadius + Vector3.up * stairs.height / 2 + Vector3.forward * stairs.depth / 2); // Convert to global space
+            Vector3 radiusHandlePosition = stairs.transform.TransformPoint(Vector3.right * stairs.innerRadius + Vector3.up * stairs.height / 2); // Convert to global space
             radiusHandlePosition = Handles.Slider(radiusHandlePosition, stairs.transform.TransformDirection(Vector3.right), settings.FindProperty("gizmoSize").floatValue, CustomHandles.DiscCapFunction, EditorSnapSettings.move.x);
 
             if (EditorGUI.EndChangeCheck())
             {
-                stairs.innerRadius = (stairs.transform.InverseTransformPoint(radiusHandlePosition) - Vector3.up * stairs.height / 2 - Vector3.forward * stairs.depth / 2).magnitude * 2; // Convert to local space
-                stairs.Generate();
-            }
-        }
-
-        widthOffset = stairs.type == Stairs.Type.Straight ? 0 : stairs.width / 2 + stairs.innerRadius;
-        // Depth handle
-        if (stairs.type == Stairs.Type.Straight)
-        {
-            EditorGUI.BeginChangeCheck();
-            Vector3 depthHandlePosition = stairs.transform.TransformPoint(Vector3.up * stairs.height / 2 + Vector3.forward * stairs.depth + Vector3.right * widthOffset); // Convert to global space
-            depthHandlePosition = Handles.Slider(depthHandlePosition, stairs.transform.TransformDirection(Vector3.forward), settings.FindProperty("gizmoSize").floatValue, CustomHandles.DiscCapFunction, EditorSnapSettings.move.z);
-
-            if (EditorGUI.EndChangeCheck())
-            {
-                stairs.depth = (stairs.transform.InverseTransformPoint(depthHandlePosition) - Vector3.up * stairs.height / 2 - Vector3.right * widthOffset).magnitude; // Convert to local space
+                stairs.innerRadius = (stairs.transform.InverseTransformPoint(radiusHandlePosition) - Vector3.up * stairs.height / 2).magnitude * 2; // Convert to local space
                 stairs.Generate();
             }
         }
 
         // Height handle
-        if (stairs.type == Stairs.Type.Straight)
         {
             EditorGUI.BeginChangeCheck();
-            Vector3 heightHandlePosition = stairs.transform.TransformPoint(Vector3.up * stairs.height + Vector3.forward * stairs.depth / 2 + Vector3.right * widthOffset); // Convert to global space
+            Vector3 heightHandlePosition = stairs.transform.TransformPoint(Vector3.up * stairs.height + Vector3.right * widthOffset); // Convert to global space
             heightHandlePosition = Handles.Slider(heightHandlePosition, stairs.transform.TransformDirection(Vector3.up), settings.FindProperty("gizmoSize").floatValue, CustomHandles.DiscCapFunction, EditorSnapSettings.move.y);
 
             if (EditorGUI.EndChangeCheck())
             {
-                stairs.height = (stairs.transform.InverseTransformPoint(heightHandlePosition) - Vector3.forward * stairs.depth / 2 - Vector3.right * widthOffset).magnitude; // Convert to local space
+                stairs.height = (stairs.transform.InverseTransformPoint(heightHandlePosition) - Vector3.right * widthOffset).magnitude; // Convert to local space
                 stairs.Generate();
             }
-        }
-        else
-        {
-
         }
     }
 }
--- Assets/Scripts/Objects/SpiralStairs.cs
+++ Assets/Scripts/Objects/SpiralStairs.cs
@@ -7,12 +7,10 @@ public class SpiralStairs : MonoBehaviour
 {
 
     // Properties
-    public float depth = 0.25f;
     public float innerRadius = 0.1f;
-    public bool rotateClockwise = true;
     public float width = 3;
-    public float height = 0.25f;
-    public Vector3 targetPosition = new Vector3(3, 1, 0);
+    public bool rotateClockwise = true;
+    public float height;
     public float targetHeight = 5f;
     public float targetRotation = 0f;
 
@@ -25,24 +23,15 @@ public class SpiralStairs : MonoBehaviour
     public void Validate()
     {
         // Validate properties
-        depth = Mathf.Clamp(depth, 0.15f, 3f);
         innerRadius = Mathf.Clamp(innerRadius, 0f, 1f);
         width = Mathf.Clamp(width, 1f, 30f);
         height = Mathf.Clamp(height, 0.15f, 1f);
+        targetHeight = Mathf.Clamp(targetHeight, 1f, 10f);
         targetRotation = Mathf.Clamp(targetRotation, 0, 1800f); // 5 turns
 
-        // Prevent scaling
+        // Prevent scaling and rotation
         transform.localScale = Vector3.one;
-
-        // Rotate object towards target
-        if (type == Type.Straight)
-        {
-            transform.rotation = Quaternion.LookRotation(Vector3Extensions.ToXZ(targetPosition));
-        }
-        else
-        {
-            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
-        }
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
     }
 
     private void GenerateMesh()
@@ -51,84 +40,43 @@ public class SpiralStairs : MonoBehaviour
         List<int> triangles = new List<int>();
         List<Vector2> uvs = new List<Vector2>();
 
-        if (type == Type.Straight)
+        int segments = (int)(targetHeight / height);
+        float actualHeight = targetHeight / segments;
+        float rotation = targetRotation / segments;
+
+        // Flip rotation direction
+        if (rotateClockwise)
         {
-            int segments = (int)(Vector3Extensions.XZDistance(Vector3.zero, targetPosition) / depth);
-            float actualDepth = Vector3Extensions.XZDistance(Vector3.zero, targetPosition) / segments;
-            AddStraightStairSide(ref vertices, ref triangles, ref uvs, segments, actualDepth, Vector3.left, 0); // Left side
-            AddStraightStairSide(ref vertices, ref triangles, ref uvs, segments, actualDepth, Vector3.right, segments * 3 + 1); // Right side
+            AddStairSide(ref vertices, ref triangles, ref uvs, segments, actualHeight, rotation, innerRadius + width, segments * 3 + 1); // Outer side
+            AddStairSide(ref vertices, ref triangles, ref uvs, segments, actualHeight, rotation, innerRadius, 0); // Inner side
         }
         else
         {
-            int segments = (int)(Vector3Extensions.XZDistance(Vector3.zero, targetPosition) / depth);
-            AddStraightStairSide(ref vertices, ref triangles, ref uvs, segments, actualDepth, Vector3.left, 0); // Left side
-            AddStraightStairSide(ref vertices, ref triangles, ref uvs, segments, actualDepth, Vector3.right, segments * 3 + 1); // Right side
+            AddStairSide(ref vertices, ref triangles, ref uvs, segments, actualHeight, -rotation, innerRadius, 0); // Inner side 
+            AddStairSide(ref vertices, ref triangles, ref uvs, segments, actualHeight, -rotation, innerRadius + width, segments * 3 + 1); // Outer side      
         }
 
         MeshTools.ConnectToNextIteration(ref triangles, 0, 1, vertices.Count / 2); // Create top/bottom faces
         MeshTools.CreateMesh(gameObject, vertices, triangles, uvs);
     }
-
-    private void AddStraightStairSide(ref List<Vector3> vertices, ref List<int> triangles, ref List<Vector2> uvs, int segments, float actualDepth, Vector3 left, int offset)
-    {
-        // Top part
-        for (int i = 0; i < segments; i++)
-        {
-            // Add start vertex .
-            if (i == 0)
-            {
-                vertices.Add(left * width / 2);
-            }
-
-            // Add top vertices :*
-            vertices.Add(Vector3.forward * actualDepth * i + Vector3.up * height * (i + 1) + left * width / 2); // Top left
-            vertices.Add(Vector3.forward * actualDepth * (i + 1) + Vector3.up * height * (i + 1) + left * width / 2); // Top right
-
-            // Add uvs
-            // Add start vertex
-            if (i == 0)
-            {
-                uvs.Add(Vector2.zero);
-            }
-
-            uvs.Add(Vector2.zero);
-            uvs.Add(Vector2.zero);
-            uvs.Add(Vector2.zero);
-        }
-
-        // Bottom part
-        for (int i = segments - 1; i >= 0; i--)
-        {
-            vertices.Add(Vector3.forward * actualDepth * (i + 1) + Vector3.up * height * i + left * width / 2); // Bottom right vertex ::
-
-            if (offset == 0)
-            {
-                // Normal in left direction
-                MeshTools.AddSquare(ref triangles, offset + i * 2, offset + i * 2 + 1, vertices.Count - 2, vertices.Count - 1);
-            }
-            else
-            {
-                // Normal in right direction
-                MeshTools.AddSquare(ref triangles, vertices.Count - 1, vertices.Count - 2, offset + i * 2 + 1, offset + i * 2);
-            }
-        }
-    }
-
-    private void AddSpiralStairSide(ref List<Vector3> vertices, ref List<int> triangles, ref List<Vector2> uvs, int segments, float actualDepth, int offset)
+    private void AddStairSide(ref List<Vector3> vertices, ref List<int> triangles, ref List<Vector2> uvs, int segments, float height, float rotation, float width, int offset)
     {
         float currentRotation = 0;
         // Top part
         for (int i = 0; i < segments; i++)
         {
+            Vector3 currentPosition = Quaternion.Euler(0, currentRotation, 0) * Vector3.forward;
+            Vector3 nextPosition = Quaternion.Euler(0, currentRotation + rotation, 0) * Vector3.forward;
+
             // Add start vertex .
             if (i == 0)
             {
-                vertices.Add(left * width / 2);
+                vertices.Add(Vector3.forward * width);
             }
 
             // Add top vertices :*
-            vertices.Add(Vector3.forward * actualDepth * i + Vector3.up * height * (i + 1) + left * width / 2); // Top left
-            vertices.Add(Vector3.forward * actualDepth * (i + 1) + Vector3.up * height * (i + 1) + left * width / 2); // Top right
+            vertices.Add(Vector3.up * height * (i + 1) + currentPosition.normalized * width); // Top left
+            vertices.Add(Vector3.up * height * (i + 1) + nextPosition.normalized * width); // Top right
 
             // Add uvs
             // Add start vertex
@@ -140,12 +88,18 @@ public class SpiralStairs : MonoBehaviour
             uvs.Add(Vector2.zero);
             uvs.Add(Vector2.zero);
             uvs.Add(Vector2.zero);
+
+            // Change variables for next iteration
+            currentRotation += rotation;
         }
 
         // Bottom part
         for (int i = segments - 1; i >= 0; i--)
         {
-            vertices.Add(Vector3.forward * actualDepth * (i + 1) + Vector3.up * height * i + left * width / 2); // Bottom right vertex ::
+            currentRotation -= rotation;
+            Vector3 nextPosition = Quaternion.Euler(0, currentRotation + rotation, 0) * Vector3.forward;
+
+            vertices.Add(Vector3.up * height * i + nextPosition.normalized * width); // Bottom right vertex ::
 
             if (offset == 0)
             {
